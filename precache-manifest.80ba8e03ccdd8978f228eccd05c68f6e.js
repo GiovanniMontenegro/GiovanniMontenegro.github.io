81658ea9da0a9548fd182d55bf350a30957d39c6
Giovanni <giovanni.montenegroit@gmail.com>
2019-02-12 23:33:55 +0100

    init commit

---
+++ precache-manifest.80ba8e03ccdd8978f228eccd05c68f6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bdefaa6dbdf5d5008381",
+    "url": "/static/js/main.bdefaa6d.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "bdefaa6dbdf5d5008381",
+    "url": "/static/css/main.7030533c.chunk.css"
+  },
+  {
+    "revision": "4148da4a85e4686fe83507996c6fb68b",
+    "url": "/index.html"
+  }
+];
